1ae9d1dd2cbf3c9933f3a51956b218456f59339f
furuta <furuta@jsk.imi.i.u-tokyo.ac.jp>
2013-11-14 10:29:23 +0000

    change msg type to actionlib_msgs

--- jsk_rviz_plugins/src/publish_topic.cpp
+++ jsk_rviz_plugins/src/publish_topic.cpp
@@ -14,25 +14,9 @@
 
 namespace jsk_rviz_plugin
 {
-
-  // BEGIN_TUTORIAL
-  // Here is the implementation of the TeleopPanel class.  TeleopPanel
-  // has these responsibilities:
-  //
-  // - Act as a container for GUI elements DriveWidget and QLineEdit.
-  // - Publish command velocities 10 times per second (whether 0 or not).
-  // - Saving and restoring internal state from a config file.
-  //
-  // We start with the constructor, doing the standard Qt thing of
-  // passing the optional *parent* argument on to the superclass
-  // constructor, and also zero-ing the velocities we will be
-  // publishing.
   PublishTopic::PublishTopic( QWidget* parent )
     : rviz::Panel( parent )
   {
-    //return;
-    // Next we lay out the "output topic" text entry field using a
-    // QLabel and a QLineEdit in a QHBoxLayout.
     QHBoxLayout* topic_layout = new QHBoxLayout;
     topic_layout->addWidget( new QLabel( "Topic:" ));
     output_topic_editor_ = new QLineEdit;
@@ -47,26 +31,12 @@ namespace jsk_rviz_plugin
     layout->addWidget( send_topic_button_ );
     setLayout( layout );
 
-    // Create a timer for sending the output.  Motor controllers want to
-    // be reassured frequently that they are doing the right thing, so
-    // we keep re-sending velocities even when they aren't changing.
-    // 
-    // Here we take advantage of QObject's memory management behavior:
-    // since "this" is passed to the new QTimer as its parent, the
-    // QTimer is deleted by the QObject destructor when this TeleopPanel
-    // object is destroyed.  Therefore we don't need to keep a pointer
-    // to the timer.
 
-    // Next we make signal/slot connections.
     connect( send_topic_button_, SIGNAL( clicked() ), this, SLOT( sendTopic ()));
     connect( output_topic_editor_, SIGNAL( editingFinished() ), this, SLOT( updateTopic() ));
 
   }
 
-  // Read the topic name from the QLineEdit and call setTopic() with the
-  // results.  This is connected to QLineEdit::editingFinished() which
-  // fires when the user presses Enter or Tab or otherwise moves focus
-  // away.
   void PublishTopic::updateTopic()
   {
     setTopic( output_topic_editor_->text() );
@@ -86,18 +56,9 @@ namespace jsk_rviz_plugin
 	  }
 	else
 	  {
-	    // The old ``velocity_publisher_`` is destroyed by this assignment,
-	    // and thus the old topic advertisement is removed.  The call to
-	    // nh_advertise() says we want to publish data on the new topic
-	    // name.
 	    velocity_publisher_ = nh_.advertise<std_msgs::Empty>( output_topic_.toStdString(), 1 );
 	  }
-	// rviz::Panel defines the configChanged() signal.  Emitting it
-	// tells RViz that something in this panel has changed that will
-	// affect a saved config file.  Ultimately this signal can cause
-	// QWidget::setWindowModified(true) to be called on the top-level
-	// rviz::VisualizationFrame, which causes a little asterisk ("*")
-	// to show in the window's title bar indicating unsaved changes.
+
 	Q_EMIT configChanged();
       }
   }
@@ -107,9 +68,7 @@ namespace jsk_rviz_plugin
     velocity_publisher_.publish(msg);
   }
 
-  // Save all configuration data from this panel to the given
-  // Config object.  It is important here that you call save()
-  // on the parent class so the class id and panel name get saved.
+
   void PublishTopic::save( rviz::Config config ) const
   {
     rviz::Panel::save( config );
@@ -128,11 +87,8 @@ namespace jsk_rviz_plugin
       }
   }
 
-} // end namespace rviz_plugin_tutorials
+}
 
-// Tell pluginlib about this class.  Every class which should be
-// loadable by pluginlib::ClassLoader must have these two lines
-// compiled in its .cpp file, outside of any namespace scope.
 #include <pluginlib/class_list_macros.h>
 PLUGINLIB_EXPORT_CLASS(jsk_rviz_plugin::PublishTopic, rviz::Panel )
-// END_TUTORIAL
+
--- jsk_rviz_plugins/src/publish_topic.h
+++ jsk_rviz_plugins/src/publish_topic.h
@@ -12,67 +12,37 @@ namespace jsk_rviz_plugin
 {
   class PublishTopic: public rviz::Panel
     {
-      // This class uses Qt slots and is a subclass of QObject, so it needs
-      // the Q_OBJECT macro.
 Q_OBJECT
   public:
-      // QWidget subclass constructors usually take a parent widget
-      // parameter (which usually defaults to 0).  At the same time,
-      // pluginlib::ClassLoader creates instances by calling the default
-      // constructor (with no arguments).  Taking the parameter and giving
-      // a default of 0 lets the default constructor work and also lets
-      // someone using the class for something else to pass in a parent
-      // widget as they normally would with Qt.
       PublishTopic( QWidget* parent = 0 );
 
-      // Now we declare overrides of rviz::Panel functions for saving and
-      // loading data from the config file.  Here the data is the
-      // topic name.
       virtual void load( const rviz::Config& config );
       virtual void save( rviz::Config config ) const;
 
-      // Next come a couple of public Qt slots.
       public Q_SLOTS:
-      // In this example setTopic() does not get connected to any signal
-      // (it is called directly), but it is easy to define it as a public
-      // slot instead of a private function in case it would be useful to
-      // some other user.
+
       void setTopic( const QString& topic );
 
-      // Here we declare some internal slots.
       protected Q_SLOTS:
-      // sendvel() publishes the current velocity values to a ROS
-      // topic.  Internally this is connected to a timer which calls it 10
-      // times per second.
+
       void sendVel();
 
-      // updateTopic() reads the topic name from the QLineEdit and calls
-      // setTopic() with the result.
       void updateTopic();
 
       void sendTopic();
 
-      // Then we finish up with protected member variables.
     protected:
-      // The control-area widget which turns mouse events into command
-      // velocities.
-
-      // One-line text editor for entering the outgoing ROS topic name.
       QLineEdit* output_topic_editor_;
 
-      // The current name of the output topic.
       QString output_topic_;
 
       QPushButton* send_topic_button_;
 
-      // The ROS publisher for the command velocity.
       ros::Publisher velocity_publisher_;
 
-      // The ROS node handle.
       ros::NodeHandle nh_;
-
     };
 
-} // end namespace rviz_plugin_tutorials
+}
 
-#endif // TELEOP_PANEL_H
+#endif
