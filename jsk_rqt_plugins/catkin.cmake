ad3b803b713272f72ec5512d0342358ea466a29e
Ryohei Ueda <garaemon@gmail.com>
2014-05-03 02:43:14 +0900

    implement 3d plotter

--- jsk_rqt_plugins/catkin.cmake
+++ jsk_rqt_plugins/catkin.cmake
@@ -7,5 +7,5 @@ catkin_python_setup()
 
 install(FILES plugin.xml
   DESTINATION ${CATKIN_PACKAGE_SHARE_DESTINATION})
-install(PROGRAMS bin
+install(PROGRAMS bin/rqt_3d_plot
   DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION})
