932b3d4885c3eaba7d54363a478dcf2a93baf505
Ryohei Ueda <garaemon@gmail.com>
2014-12-12 02:02:11 +0900

    Compute difference to mouse position

--- jsk_rviz_plugins/scripts/rviz_mouse_point_to_tablet.py
+++ jsk_rviz_plugins/scripts/rviz_mouse_point_to_tablet.py
@@ -6,30 +6,51 @@ from math import pi
 from view_controller_msgs.msg import CameraPlacement
 from geometry_msgs.msg import PointStamped
 from jsk_gui_msgs.msg import Tablet
+from threading import Lock
 
-latest_camera_placement = None
+lock = Lock()
+prev_mouse_point = None
+latest_mouse_point = None
+
 def pointCallback(msg):
-    global latest_camera_placement
-    latest_camera_placement = msg
+    global latest_mouse_point
+    with lock:
+        latest_mouse_point = msg
     
 def timerCallback(event):
-    global latest_camera_placement
-    if not latest_camera_placement:
-        return
-    msg = Tablet()
-    msg.header.stamp = rospy.Time.now()
-    msg.action.task_name = "MoveCameraCenter"
-    # chop
-    msg.action.touch_x = 640 * latest_camera_placement.point.x
-    msg.action.touch_y = 480 * latest_camera_placement.point.y
-    print (msg.action.touch_x, msg.action.touch_y)
-    pub.publish(msg)
-    latest_camera_placement = None
-        
-rospy.init_node("rviz_mouse_point_to_tablet")
-pub = rospy.Publisher("/Tablet/Command", Tablet)
+    global latest_mouse_point, prev_mouse_point
+    # print (latest_mouse_point and latest_mouse_point.point, 
+    #        prev_mouse_point and prev_mouse_point.point)
+    with lock:
+        if not latest_mouse_point:
+            return
+        if not prev_mouse_point:
+            prev_mouse_point = latest_mouse_point
+            latest_mouse_point = None
+            return
+        # compute diff
+        diff_x = (latest_mouse_point.point.x - prev_mouse_point.point.x) * 640
+        diff_y = (latest_mouse_point.point.y - prev_mouse_point.point.y) * 480
+        # center is 320, 240
+        next_x = diff_x + 320
+        next_y = diff_y + 240
+        msg = Tablet()
+        msg.header.stamp = rospy.Time.now()
+        msg.action.task_name = "MoveCameraCenter"
+        # chop
+        msg.action.touch_x = next_x
+        msg.action.touch_y = next_y
+        rospy.loginfo("neck actino to (%f, %f)" % (msg.action.touch_x, msg.action.touch_y))
+        pub.publish(msg)
+        #prev_mouse_point = latest_mouse_point
+        prev_mouse_point = None
+        latest_mouse_point = None
 
-sub = rospy.Subscriber("/rviz/current_mouse_point", 
-                       PointStamped, pointCallback)
-rospy.Timer(rospy.Duration(0.5), timerCallback)
-rospy.spin()
+        
+if __name__ == "__main__":
+    rospy.init_node("rviz_mouse_point_to_tablet")
+    pub = rospy.Publisher("/Tablet/Command", Tablet)
+    sub = rospy.Subscriber("/rviz/current_mouse_point", 
+                           PointStamped, pointCallback)
+    rospy.Timer(rospy.Duration(0.3), timerCallback)
+    rospy.spin()
