df9e94d9dcf1a4f9ee8106e8787be2fb3b819f1e
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2017-07-06 00:22:29 +0900

    [jsk_rviz_plugins] add visualizer for classification result

--- jsk_rviz_plugins/scripts/classification_result_visualizer.py
+++ jsk_rviz_plugins/scripts/classification_result_visualizer.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 # Author: Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
 
+from dynamic_reconfigure.server import Server
 import rospy
 from jsk_topic_tools import ConnectionBasedTransport
 from jsk_recognition_msgs.msg import BoundingBoxArray
 from jsk_recognition_msgs.msg import ClassificationResult
+from jsk_rviz_plugins.cfg import ClassificationResultVisualizerConfig
 import message_filters
 from std_msgs.msg import ColorRGBA
 from visualization_msgs.msg import Marker, MarkerArray
@@ -14,10 +16,12 @@ from visualization_msgs.msg import Marker, MarkerArray
 class ClassificationResultVisualizer(ConnectionBasedTransport):
     def __init__(self):
         super(ClassificationResultVisualizer, self).__init__()
+
         self.queue_size = rospy.get_param("~queue_size", 100)
-        self.text_color = rospy.get_param("~text_color", [0.0, 0.0, 1.0, 1.0])
-        self.text_offset = rospy.get_param("~text_offset", [0.0, 0.0, 0.07])
-        self.text_size = rospy.get_param("~text_size", 0.05)
+
+        self.srv = Server(ClassificationResultVisualizerConfig,
+                          self.config_callback)
+
         self.pub_marker = self.advertise("~output", MarkerArray, queue_size=10)
 
     def subscribe(self):
@@ -26,27 +30,38 @@ class ClassificationResultVisualizer(ConnectionBasedTransport):
             message_filters.Subscriber("~input/classes", ClassificationResult),
         ]
         self.sync = message_filters.TimeSynchronizer(self.subscribers, self.queue_size)
-        self.sync.registerCallback(self.callback)
+        self.sync.registerCallback(self.msg_callback)
 
     def unsubscribe(self):
         for sub in self.subscribers:
             sub.unregister()
 
-    def callback(self, bbox, cls):
+    def config_callback(self, config, level):
+        self.text_color = {'r': config.text_color_red,
+                           'g': config.text_color_green,
+                           'b': config.text_color_blue,
+                           'a': config.text_color_alpha}
+        self.text_offset = [config.text_offset_x,
+                            config.text_offset_y,
+                            config.text_offset_z]
+        self.text_size = config.text_size
+        self.marker_lifetime = config.marker_lifetime
+        return config
+
+    def msg_callback(self, bboxes, classes):
         msg = MarkerArray()
-        for i, data in enumerate(zip(bbox.boxes, zip(cls.label_names, cls.label_proba))):
-            b, c = data
-            text = "%s (%.3f)" % c
+        for i, data in enumerate(zip(bboxes.boxes, zip(classes.label_names, classes.label_proba))):
+            bbox, cls = data
+            text = "%s (%.3f)" % cls
             m = Marker(type=Marker.TEXT_VIEW_FACING,
-                       action=Marker.ADD,
+                       action=Marker.MODIFY,
                        header=bbox.header,
                        id=i,
-                       pose=b.pose,
-                       color=ColorRGBA(self.text_color[0], self.text_color[1],
-                                       self.text_color[2], self.text_color[3]),
+                       pose=bbox.pose,
+                       color=ColorRGBA(*self.text_color),
                        text=text,
-                       ns=cls.classifier,
-                       lifetime=rospy.Time(30))
+                       ns=classes.classifier,
+                       lifetime=rospy.Time(self.marker_lifetime))
             m.scale.z = self.text_size
             m.pose.position.x += self.text_offset[0]
             m.pose.position.y += self.text_offset[1]
