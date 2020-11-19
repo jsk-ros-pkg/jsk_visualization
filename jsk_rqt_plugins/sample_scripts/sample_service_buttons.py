8242bfba3dbe5583419753b4e56e922ca5e0953b
Shingo Kitagawa <shingogo@hotmail.co.jp>
2020-11-19 13:36:24 +0900

    sample_service_button.py support SetBool

--- jsk_rqt_plugins/sample_scripts/sample_service_buttons.py
+++ jsk_rqt_plugins/sample_scripts/sample_service_buttons.py
@@ -4,18 +4,30 @@ import rospy
 
 from std_srvs.srv import Empty
 from std_srvs.srv import EmptyResponse
+from std_srvs.srv import SetBool
+from std_srvs.srv import SetBoolResponse
+from std_srvs.srv import Trigger
+from std_srvs.srv import TriggerResponse
 
 
 class SampleServiceButtons(object):
     def __init__(self):
         self.services = [
-            rospy.Service('dummy/buttonA', Empty, self._empty_cb),
-            rospy.Service('dummy/buttonB', Empty, self._empty_cb),
-            rospy.Service('dummy/buttonC', Empty, self._empty_cb),
-            rospy.Service('dummy/buttonD', Empty, self._empty_cb),
+            rospy.Service('dummy/buttonA', SetBool, self._set_bool_cb),
+            rospy.Service('dummy/buttonB', SetBool, self._set_bool_cb),
+            rospy.Service('dummy/buttonC', SetBool, self._set_bool_cb),
+            rospy.Service('dummy/buttonD', Trigger, self._trigger_cb),
             rospy.Service('dummy/buttonE', Empty, self._empty_cb),
             rospy.Service('dummy/buttonF', Empty, self._empty_cb),
         ]
+
+    def _set_bool_cb(self, req):
+        rospy.loginfo('SetBool service called: req.data={}'.format(req.data))
+        return SetBoolResponse(success=True)
+
+    def _trigger_cb(self, req):
+        rospy.loginfo('Trigger service called')
+        return TriggerResponse(success=True)
 
     def _empty_cb(self, req):
         rospy.loginfo('Empty service called')
