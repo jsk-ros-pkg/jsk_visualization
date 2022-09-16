9051cfabcf5e42847c97536e1c67880e19abf16e
iory <ab.ioryz@gmail.com>
2022-09-16 22:19:23 +0900

    Fixed typo of Software License Agreement. and/o2r to and/or

--- jsk_rviz_plugins/src/torus_array_display.cpp
+++ jsk_rviz_plugins/src/torus_array_display.cpp
@@ -12,7 +12,7 @@
  *     notice, this list of conditions and the following disclaimer.
  *   * Redistributions in binary form must reproduce the above
  *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/o2r other materials provided
+ *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
  *   * Neither the name of the JSK Lab nor the names of its
  *     contributors may be used to endorse or promote products derived
